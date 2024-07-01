b87154b49d71fb29befc9b8df56dd86805f23034
Victor Alessander <alessander@protonmail.com>
2024-06-30 23:36:11 -0300

    feat: adding eruby to autotag and none_ls

--- after/plugin/autotag.lua
+++ after/plugin/autotag.lua
@@ -1,9 +1,9 @@
-require("nvim-treesitter.configs").setup({
+require("nvim-ts-autotag").setup({
 	autotag = {
 		enable = true,
 		enable_rename = true,
 		enable_close = true,
 		enable_close_on_slash = true,
-		filetypes = { "html", "xml", "typescriptreact", "javascriptreact", "tsx", "jsx" },
+		filetypes = { "html", "xml", "typescriptreact", "javascriptreact", "tsx", "jsx", "eruby" },
 	},
 })
--- after/plugin/none_ls.lua
+++ after/plugin/none_ls.lua
@@ -21,6 +21,7 @@ null_ls.setup({
 
 	sources = {
 		null_ls.builtins.formatting.stylua,
+		null_ls.builtins.formatting.erb_format,
 		require("none-ls.diagnostics.eslint"),
 		null_ls.builtins.formatting.prettier,
 		null_ls.builtins.formatting.black.with({
