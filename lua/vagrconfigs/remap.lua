fc5c2c1750d628a21557c1232a044046c242f1bc
Victor Alessander <victor.rocha@paymentworks.com>
2024-08-25 15:29:57 -0300

    updated

--- lua/vagrconfigs/remap.lua
+++ lua/vagrconfigs/remap.lua
@@ -4,5 +4,4 @@ vim.keymap.set("v", "K", ":m '<-2<CR>gv=gv")
 vim.keymap.set("n", "J", "mzJ`z")
 
 vim.keymap.set("n", "<S-f>", "<cmd>:!black % --quiet --line-length=120<CR>")
-vim.keymap.set("n", ":e", ":Explore")
 vim.keymap.set("n", "<S-r>", ":%bd|e#<CR>")
